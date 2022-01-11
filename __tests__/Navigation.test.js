739b9a98ff8e538826fde5072a330172c779c1cc
Emmanuel Márquez <emmanuelmr1@gmail.com>
2022-01-11 10:06:17 -0500

    fix: avoid import of file after test
    fixes #2

--- __tests__/Navigation.test.js
+++ __tests__/Navigation.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Button, View } from 'react-native';
-import { act, fireEvent, render } from '@testing-library/react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Button, View} from 'react-native';
+import {act, fireEvent, render} from '@testing-library/react-native';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
@@ -10,23 +10,25 @@ it('just succeed', () => {
   expect(true).toBe(true);
 });
 
-const ScreenFactory = (screenName) => ({ navigation }) => {
-  return (
-    <View>
-      <Button
-        title={`Go to ${screenName}`}
-        onPress={() => {
-          navigation.navigate(screenName);
-        }}
-      />
-    </View>
-  );
-};
+const ScreenFactory =
+  screenName =>
+  ({navigation}) => {
+    return (
+      <View>
+        <Button
+          title={`Go to ${screenName}`}
+          onPress={() => {
+            navigation.navigate(screenName);
+          }}
+        />
+      </View>
+    );
+  };
 
 const ScreenA = ScreenFactory('Screen B');
 const ScreenB = ScreenFactory('Screen A');
 
-const { Screen, Navigator } = createStackNavigator();
+const {Screen, Navigator} = createStackNavigator();
 
 const App = () => {
   return (
@@ -39,10 +41,12 @@ const App = () => {
   );
 };
 
+jest.useFakeTimers();
+
 describe('Navigation Container', () => {
   it('run test without importing a file after it', async () => {
-    const { getByText } = render(<App />);
-    await act(async () => {});
+    const {getByText} = render(<App />);
+    act(() => jest.runAllTimers());
 
     getByText('Go to Screen B');
   });
